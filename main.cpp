b777c92230920c4d01d5e5dce00454724fd24263
SMFTLK <128456105+SMFTLK@users.noreply.github.com>
2023-04-23 17:59:09 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "math.h"
+
+int main()
+{
+	fibonacci_init(1, 1);
+	do {
+		std::cout << fibonacci_index() << ": "
+			<< fibonacci_current() << std::endl;
+	} while (fibonacci_next());
+	std::cout << fibonacci_index() + 1 <<
+		" Значения последовательности Фибоначчи вписываются в " <<
+		"unsigned 64-bit integer." << std::endl;
+}
